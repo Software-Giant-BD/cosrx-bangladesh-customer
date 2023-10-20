0de0b51c2c057f4ca61eefb024947cac6ab7ea71
Nafis Chonchol <nafisislam161@gmail.com>
2023-10-20 10:47:23 +0600

    update

--- resources/views/components/category-list.blade.php
+++ resources/views/components/category-list.blade.php
@@ -1,7 +1,28 @@
-    <li class="has-submenu"><a href="blog.html">Product Type</a>
-        <ul class="submenu-nav">
-            @foreach ($categories as $item)
-                <li><a href="blog-details.html">{{$item->name}}</a></li>
+@if (count($data)>0)
+    <li class="has-submenu position-static"><a href="product.html">Product Type</a>
+        <ul class="submenu-nav-mega">
+            @foreach($data as $index => $item)
+                @if ($index % 7 == 0)
+                    @if ($index>0)  {{-- close previous li and ul --}}
+                        </ul>  
+                    </li>
+                    @endif
+                    <li>
+                        <ul>
+                            <li><a href="#">{{$item->name}}</a></li>
+                @elseif($index == count($data)-1) {{-- close last li and ul --}}
+                    </ul>
+                </li>
+                @else
+                    <li><a href="#">{{$item->name}}</a></li>
+                @endif
             @endforeach
         </ul>
     </li>
+@else
+    <li class="has-submenu"><a href="blog.html">Product Type</a>
+        <ul class="submenu-nav p-2">
+            <li>No items to display</li>
+        </ul>
+    </li>
+@endif
