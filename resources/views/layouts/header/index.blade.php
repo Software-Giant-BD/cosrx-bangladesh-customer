d958b1773263b5418c82f918aa1f482cbdf7354a
Nafis Chonchol <nafisislam161@gmail.com>
2023-10-24 09:08:15 +0600

    create files and separate

--- resources/views/layouts/header/index.blade.php
+++ resources/views/layouts/header/index.blade.php
@@ -49,23 +49,7 @@
                                 </svg>
                             </span>
                         </button>
-
-                        <button class="header-action-btn d-flex justify-content-between align-items-center"
-                            type="button" data-bs-toggle="offcanvas" data-bs-target="#AsideOffcanvasCart"
-                            aria-controls="AsideOffcanvasCart">
-                            <span class="me-2">Cart</span>
-                            <span class="icon">
-                                <svg width="24px" height="24px" viewBox="0 0 24 24" aria-hidden="true">
-                                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-                                        <polygon stroke="currentColor" stroke-width="1.5"
-                                            points="2 9.25 22 9.25 18 21.25 6 21.25"></polygon>
-                                        <line x1="12" y1="9" x2="12" y2="3"
-                                            stroke="currentColor" stroke-width="1.5" stroke-linecap="square"></line>
-                                    </g>
-                                </svg>
-                            </span>
-                            <span class="cart-value" id="cart-value">43</span>
-                        </button>
+                        @include("layouts.header.cart")
 
                     </div>
                 </div>
