356760ee82eba40fd80f5b1e780399497a252e15
Nafis Chonchol <nafisislam161@gmail.com>
2023-10-29 20:51:05 +0600

    cart design

---
+++ resources/views/cart/index.blade.php
@@ -0,0 +1,129 @@
+@extends('layouts.index')
+@section('main')
+<main class="main-content">
+    @php
+        $cart = session('cart');
+        $coupon = session('coupon');
+        $coupon_discount = 0;
+        if (isset($coupon['coupon_discount'])) {
+            $coupon_discount = $coupon['coupon_discount'];
+        }
+        $subTotal = 0;
+        $total = 0;
+    @endphp
+
+    <!--== Start Page Header Area Wrapper ==-->
+    <nav aria-label="breadcrumb" class="breadcrumb-style1">
+        <div class="container">
+            <ol class="breadcrumb justify-content-center">
+                <li class="breadcrumb-item"><a href="{{route('home')}}">Home</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Cart</li>
+            </ol>
+        </div>
+    </nav>
+
+    <!--== Start Product Area Wrapper ==-->
+    <section class="section-space">
+        <div class="container">
+            <div class="shopping-cart-form table-responsive">
+                
+                    <table class="table text-center">
+                        <thead>
+                            <tr>
+                                <th class="product-remove">&nbsp;</th>
+                                <th class="product-thumbnail">&nbsp;</th>
+                                <th class="product-name">Product</th>
+                                <th class="product-price">Price</th>
+                                <th class="product-quantity">Quantity</th>
+                                <th class="product-subtotal">Total</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($cart as $item)
+                            <tr class="tbody-item">
+                                <td class="product-remove">
+                                    <a class="remove" href="javascript:void(0)">×</a>
+                                </td>
+                                <td class="product-thumbnail">
+                                    <div class="thumb">
+                                        <a href="single-product.html">
+                                            <img src="{{env('Admin_url_public').$item['product_image']}}" width="68" height="84" alt="Image-HasTech">
+                                        </a>
+                                    </div>
+                                </td>
+                                <td class="product-name">
+                                    <a class="title" href="single-product.html">{{$item['product_name']}}</a>
+                                </td>
+                                <td class="product-price">
+                                    <span class="price">${{$item['product_price']}}</span>
+                                </td>
+                                <td class="product-quantity">
+                                    <div class="pro-qty">
+                                        <input type="text" class="quantity" title="Quantity" value="{{$item['qty']}}">
+                                    </div>
+                                </td>
+                                <td class="product-subtotal">
+                                    <span class="price">${{$item['product_price'] * $item['qty']}}</span>
+                                </td>
+                            </tr>
+                            @endforeach
+                            <tr class="tbody-item-actions">
+                                <td colspan="6">
+                                    <a type="submit" class="btn-update-cart disabled" disabled href="{{route('home')}}">Continue Shopping</a>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+               
+            </div>
+            <div class="row">
+                <div class="col-12 col-lg-6">
+                    <div class="coupon-wrap">
+                        <h4 class="title">Coupon</h4>
+                        <p class="desc">Enter your coupon code if you have one.</p>
+                        <input type="text" class="form-control" placeholder="Coupon code">
+                        <button type="button" class="btn-coupon">Apply coupon</button>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-6">
+                    <div class="cart-totals-wrap">
+                        <h2 class="title">Cart totals</h2>
+                        <table>
+                            <tbody>
+                                <tr class="cart-subtotal">
+                                    <th>Subtotal</th>
+                                    <td>
+                                        <span class="amount">$499.00</span>
+                                    </td>
+                                </tr>
+                                <tr class="shipping-totals">
+                                    <th>Coupon</th>
+                                    <td>
+                                        <span class="amount">$499.00</span>
+                                    </td>
+                                </tr>
+                                <tr class="order-total">
+                                    <th>Total</th>
+                                    <td>
+                                        <span class="amount">$504.00</span>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <div class="text-end">
+                            <a href="shop-checkout.html" class="checkout-button">Proceed to checkout</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--== End Product Area Wrapper ==-->
+
+</main>
+@endsection
+
+@section('js')
+    @include('mgs.sweet-alert')
+   
+@endsection
