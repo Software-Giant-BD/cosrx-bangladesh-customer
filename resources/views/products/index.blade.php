cef27b70c349ae19ee565b986aac63bae9c24efc
Nafis Chonchol <nafisislam161@gmail.com>
2023-10-29 18:23:01 +0600

    ok

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -10,7 +10,7 @@
                 <div class="row product-details">
                     <div class="col-lg-6">
                         <div class="product-details-thumb">
-                            <img src="{{ env('Admin_url_public').$product->image }}" width="570" height="693"
+                            <img src="{{ env('Admin_url_public') . $product->image }}" width="570" height="693"
                                 alt="{{ $product->img_alt }}" title="{{ $product->img_title }}">
                             <span class="flag-new">new</span>
                         </div>
@@ -21,19 +21,20 @@
                             <h3 class="product-details-title">{{ $product->name }}</h3>
                             <div class="product-details-review">
                                 <div class="product-review-icon">
-                                    @for($i=1;$i<=5;$i++)
-                                        @if ($i> $product->rating)
+                                    @for ($i = 1; $i <= 5; $i++)
+                                        @if ($i > $product->rating)
                                             <i class="fa fa-star-o"></i>
                                         @else
                                             <i class="fa fa-star"></i>
                                         @endif
                                     @endfor
                                 </div>
-                                <button type="button" class="product-review-show">{{ $product->review_count }} reviews</button>
+                                <button type="button" class="product-review-show">{{ $product->review_count }}
+                                    reviews</button>
                             </div>
                             <div class="product-details-qty-list">
                                 <div class="qty-list-check">
-                                    <p>{{$product->short_description}}</p>
+                                    <p>{{ $product->short_description }}</p>
                                 </div>
                             </div>
                             <div class="product-details-pro-qty">
@@ -42,9 +43,9 @@
                                 </div>
                             </div>
                             <div class="product-details-action">
-                                <h4 class="price details-price"> 
-                                    <span class="price mr-3">৳{{$product->discount}}</span>
-                                    <span class="price-old">৳{{$product->price}}</span>
+                                <h4 class="price details-price">
+                                    <span class="price mr-3">৳{{ $product->discount }}</span>
+                                    <span class="price-old">৳{{ $product->price }}</span>
                                 </h4>
                                 <div class="product-details-cart-wishlist">
                                     <button type="button" class="btn-wishlist" data-bs-toggle="modal"
@@ -66,45 +67,50 @@
                                 type="button" role="tab" aria-controls="review" aria-selected="true">Review</button>
                         </div>
                         <div class="tab-content" id="product-details-nav-tabContent">
-                            <div class="tab-pane" id="specification" role="tabpanel"
-                                aria-labelledby="specification-tab">
+                            <div class="tab-pane" id="specification" role="tabpanel" aria-labelledby="specification-tab">
                                 <p>{!! $product->long_description !!}</p>
                             </div>
 
                             <div class="tab-pane fade show active" id="review" role="tabpanel"
                                 aria-labelledby="review-tab">
-                                @foreach ($reviews as $item)
-                                <div class="product-review-item">
-                                    <div class="product-review-top">
-                                        <div class="product-review-thumb">
-                                            <img src="{{ asset("assets/images/shop/product-details/comment1.webp") }}" alt="Images">
-                                        </div>
-                                        <div class="product-review-content">
-                                            <span class="product-review-name">{{ $item->customer?->name }}</span>
-                                           
-                                            <div class="product-review-icon">
-                                                @for($i=1;$i<=5;$i++)
-                                                    @if ($i> $item->star)
-                                                        <i class="fa fa-star-o"></i>
-                                                    @else
-                                                        <i class="fa fa-star"></i>
-                                                    @endif
-                                                @endfor
+
+                                @forelse($reviews as $item)
+                                    <div class="product-review-item">
+                                        <div class="product-review-top">
+                                            <div class="product-review-thumb">
+                                                <img src="{{ asset('assets/images/shop/product-details/comment1.webp') }}"
+                                                    alt="Images">
+                                            </div>
+                                            <div class="product-review-content">
+                                                <span class="product-review-name">{{ $item->customer?->name }}</span>
+
+                                                <div class="product-review-icon">
+                                                    @for ($i = 1; $i <= 5; $i++)
+                                                        @if ($i > $item->star)
+                                                            <i class="fa fa-star-o"></i>
+                                                        @else
+                                                            <i class="fa fa-star"></i>
+                                                        @endif
+                                                    @endfor
+                                                </div>
                                             </div>
                                         </div>
+                                        <h5>{{ $item->title }}</h5>
+                                        <p class="desc">{{ $item->review }}</p>
                                     </div>
-                                    <h5>{{ $item->title }}</h5>
-                                    <p class="desc">{{$item->review}}</p>
-                                </div>
-                                @endforeach
+                                @empty
+                                    <div class="product-review-item">
+                                        Don't have review
+                                    </div>
+                                @endforelse
                             </div>
                         </div>
                     </div>
-                    @include("products.submit-review")
+                    @include('products.submit-review')
                 </div>
             </div>
         </section>
-        @include("products.related-products")
+        @include('products.related-products')
     </main>
 @endsection
 @section('js')
--- resources/views/products/related-products.blade.php
+++ resources/views/products/related-products.blade.php
@@ -1,3 +1,4 @@
+@if(isset($related_products[0]))
 <section class="section-space">
     <div class="container">
         <div class="row">
@@ -10,67 +11,66 @@
             <div class="col-12">
                 <div class="swiper related-product-slide-container">
                     <div class="swiper-wrapper">
-                        @if (isset($related_products[0]))
-                            @foreach ($related_products as $item)
-                            <div class="swiper-slide mb-10">
-                                <div class="product-item product-st2-item">
-                                    <div class="product-thumb">
-                                        <a class="d-block" href="{{ route('product.details',['slug'=>$item->slug]) }}">
-                                            <img src="{{ env('Admin_url_public').$item->image }}" width="370" height="450"
-                                                alt="{{ $item->img_alt }}" title="{{ $item->img_title }}">
-                                        </a>
+                        @foreach ($related_products as $item)
+                        <div class="swiper-slide mb-10">
+                            <div class="product-item product-st2-item">
+                                <div class="product-thumb">
+                                    <a class="d-block" href="{{ route('product.details',['slug'=>$item->slug]) }}">
+                                        <img src="{{ env('Admin_url_public').$item->image }}" width="370" height="450"
+                                            alt="{{ $item->img_alt }}" title="{{ $item->img_title }}">
+                                    </a>
+                                </div>
+                                <div class="product-info">
+                                    <div class="product-rating">
+                                        <div class="rating">
+                                            @for($i=1;$i<=5;$i++)
+                                                @if ($i> $item->rating)
+                                                    <i class="fa fa-star-o"></i>
+                                                @else
+                                                    <i class="fa fa-star"></i>
+                                                @endif
+                                            @endfor
+                                        </div>
+                                        <div class="reviews">{{$item->review_count}} reviews</div>
                                     </div>
-                                    <div class="product-info">
-                                        <div class="product-rating">
-                                            <div class="rating">
-                                                @for($i=1;$i<=5;$i++)
-                                                    @if ($i> $item->rating)
-                                                        <i class="fa fa-star-o"></i>
-                                                    @else
-                                                        <i class="fa fa-star"></i>
-                                                    @endif
-                                                @endfor
-                                            </div>
-                                            <div class="reviews">{{$item->review_count}} reviews</div>
-                                        </div>
-                                        <h4 class="title"><a href="{{ route('product.details',['slug'=>$item->slug]) }}">{{$item->name}}</a></h4>
-                                        <div class="prices">
-                                            <span class="price">৳{{$item->discount}}</span>
-                                            <span class="price-old">৳{{$item->price}}</span>
-                                        </div>
-                                        <div class="product-action">
-                                            <button type="button" class="product-action-btn action-btn-cart cart_add_btn" data-qty="1" data-product_id="{{ $item->id }}">
-                                                <span>Add to cart</span>
-                                            </button>
-                                            <button type="button" data-product="{{ json_encode($item) }}" class="product-action-btn action-btn-quick-view"
-                                                data-bs-toggle="modal" data-bs-target="#action-QuickViewModal">
-                                                <i class="fa fa-expand"></i>
-                                            </button>
-                                            <button type="button" class="product-action-btn action-btn-wishlist wish_add_btn">
-                                                <i class="fa fa-heart-o"></i>
-                                            </button>
-                                        </div>
-                                        <div class="product-action-bottom">
-                                            <button type="button" data-product="{{ json_encode($item) }}" class="product-action-btn action-btn-quick-view" 
-                                                data-bs-toggle="modal" data-bs-target="#action-QuickViewModal">
-                                                <i class="fa fa-expand"></i>
-                                            </button>
-                                            <button type="button" class="product-action-btn action-btn-wishlist wish_add_btn">
-                                                <i class="fa fa-heart-o"></i>
-                                            </button>
-                                            <button type="button" class="product-action-btn action-btn-cart cart_add_btn" data-qty="1" data-product_id="{{ $item->id }}">
-                                                <span>Add to cart</span>
-                                            </button>
-                                        </div>
+                                    <h4 class="title"><a href="{{ route('product.details',['slug'=>$item->slug]) }}">{{$item->name}}</a></h4>
+                                    <div class="prices">
+                                        <span class="price">৳{{$item->discount}}</span>
+                                        <span class="price-old">৳{{$item->price}}</span>
+                                    </div>
+                                    <div class="product-action">
+                                        <button type="button" class="product-action-btn action-btn-cart cart_add_btn" data-qty="1" data-product_id="{{ $item->id }}">
+                                            <span>Add to cart</span>
+                                        </button>
+                                        <button type="button" data-product="{{ json_encode($item) }}" class="product-action-btn action-btn-quick-view"
+                                            data-bs-toggle="modal" data-bs-target="#action-QuickViewModal">
+                                            <i class="fa fa-expand"></i>
+                                        </button>
+                                        <button type="button" class="product-action-btn action-btn-wishlist wish_add_btn">
+                                            <i class="fa fa-heart-o"></i>
+                                        </button>
+                                    </div>
+                                    <div class="product-action-bottom">
+                                        <button type="button" data-product="{{ json_encode($item) }}" class="product-action-btn action-btn-quick-view" 
+                                            data-bs-toggle="modal" data-bs-target="#action-QuickViewModal">
+                                            <i class="fa fa-expand"></i>
+                                        </button>
+                                        <button type="button" class="product-action-btn action-btn-wishlist wish_add_btn">
+                                            <i class="fa fa-heart-o"></i>
+                                        </button>
+                                        <button type="button" class="product-action-btn action-btn-cart cart_add_btn" data-qty="1" data-product_id="{{ $item->id }}">
+                                            <span>Add to cart</span>
+                                        </button>
                                     </div>
                                 </div>
-                                <!--== End prPduct Item ==-->
                             </div>
-                            @endforeach
-                        @endif
+                            <!--== End prPduct Item ==-->
+                        </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </section>
+@endif
